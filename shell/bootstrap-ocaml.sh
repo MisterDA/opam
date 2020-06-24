aebdd824ccaa0adc525fe2be4f1f5beacdabc6bd
Antonin Décimo <antonin.decimo@gmail.com>
2020-06-24 15:48:52 +0200

    shellcheck SC2166: use && and || instead of -a and -o

--- shell/bootstrap-ocaml.sh
+++ shell/bootstrap-ocaml.sh
@@ -38,7 +38,7 @@ cd "${V}"
 PATH_PREPEND=
 LIB_PREPEND=
 INC_PREPEND=
-if [ -n "$1" -a -n "${COMSPEC}" -a -x "${COMSPEC}" ] ; then
+if [ -n "$1" ] && [ -n "${COMSPEC}" ] && [ -x "${COMSPEC}" ] ; then
   case "$1" in
     "mingw"|"mingw64")
       BUILD=$1
@@ -69,7 +69,7 @@ if [ -n "$1" -a -n "${COMSPEC}" -a -x "${COMSPEC}" ] ; then
       if [ "$1" != "auto" ] ; then
         echo "Compiler architecture $1 not recognised -- mingw64, mingw, msvc64, msvc (or auto)"
       fi
-      if [ -n "${PROCESSOR_ARCHITEW6432}" -o "${PROCESSOR_ARCHITECTURE}" = "AMD64" ] ; then
+      if [ -n "${PROCESSOR_ARCHITEW6432}" ] || [ "${PROCESSOR_ARCHITECTURE}" = "AMD64" ] ; then
         TRY64=1
       else
         TRY64=0
